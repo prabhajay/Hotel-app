5fbfff4dc34ea7016644e8bad7dd7396e1217927
Lakshmiprabha <lakshmi.prbh@gmail.com>
2024-10-05 11:39:00 -0600

    added port number

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const express=require('express')
 //firebase-admin
 const fs = require('fs');
 const admin= require('firebase-admin')
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 5000;
 const app = express()
 const cookieParser =require('cookie-parser')
 const authRoute=require('../api/routes/auth.js')
